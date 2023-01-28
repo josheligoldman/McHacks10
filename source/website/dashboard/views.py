2fba2721ed3e1382e5760feac1f79cd303909914
josheligoldman <josheligoldman@gmail.com>
2023-01-28 13:02:32 -0500

    Templates work.

--- source/website/dashboard/views.py
+++ source/website/dashboard/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, World! You're at the dashboard index.")
+    template = loader.get_template('dashboard/index.html')
+    return HttpResponse(template.render({}, request))
 
